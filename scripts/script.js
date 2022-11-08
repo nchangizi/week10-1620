7faef3853c4e6184b81b0e63633ad26480e9c9c8
Neda <nedachangizi@gmail.com>
2022-11-08 15:15:14 -0800

    do/while

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,22 @@
 //     console.log(i)
 // }
 
+// for (let i = 1; i <= 10; i++) {
+//   console.log(Math.pow(2, i));
+// }
 
-for (let i = 1 ; i <= 10 ; i++)
-{
-    console.log(2 **i);
-}
+// let i = 1;
+// while (i <= 10) {
+//   console.log(2 ** i);
+//   i++;
+// }
+let input;
+do {
+  input = prompt("what is your name?");
+  //prompt always returns string
+  //convert input to number
+//   console.log(Number(input));
+} while (!isNaN(Number(input)));
 
-let i = 1;
-while (i <= 10)
-{
-    console.log(2 **i);
-    i++;
-}
+// alert("Hello " + input );
+alert(`Hello ${input}`);
