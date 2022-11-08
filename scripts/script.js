c38d39898b2be1df4a124beb93d2c013da488a81
Neda <nedachangizi@gmail.com>
2022-11-08 15:40:46 -0800

    array methods

--- scripts/script.js
+++ scripts/script.js
@@ -12,13 +12,28 @@
 //   console.log(2 ** i);
 //   i++;
 // }
-let input;
-do {
-  input = prompt("what is your name?");
-  //prompt always returns string
-  //convert input to number
-//   console.log(Number(input));
-} while (!isNaN(Number(input)));
+// let input;
+// do {
+//   input = prompt("what is your name?");
+//   //prompt always returns string
+//   //convert input to number
+// //   console.log(Number(input));
+// } while (!isNaN(Number(input)));
 
-// alert("Hello " + input );
-alert(`Hello ${input}`);
+// // alert("Hello " + input );
+// alert(`Hello ${input}`);
+
+let fruits=["Apple", "Banana"];
+let pushResult = fruits.push("Orange");
+console.log(pushResult);
+
+let popResult = fruits.pop();
+console.log(popResult);
+
+// fruits.unshift("Mango");
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+
+// console.log(fruits.indexOf("Apple"))
