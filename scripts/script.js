8bb802d1df2732b28b6d170d9db0f81edfe194eb
Neda <nedachangizi@gmail.com>
2022-11-08 14:48:15 -0800

    while and for

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// for (let i = 0; i < 1000000; i++)
+// {
+//     console.log(i)
+// }
+
+
+for (let i = 1 ; i <= 10 ; i++)
+{
+    console.log(2 **i);
+}
+
+let i = 1;
+while (i <= 10)
+{
+    console.log(2 **i);
+    i++;
+}
