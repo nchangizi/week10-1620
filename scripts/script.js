5392f11167520f6d108d3708cfa73bc6db1f0a10
Neda <nedachangizi@gmail.com>
2022-11-08 16:12:19 -0800

    acitvity

--- scripts/script.js
+++ scripts/script.js
@@ -51,8 +51,34 @@ const person = {
 //   `My name is ${person.name[0]} ${person.name[1]}. I am intereseted in ${person.hobbies[0]} and ${person.hobbies[1]}`
 // );
 
-for (let key in person)
-{
-    console.log(key);
-    console.log(person[key]);
+// for (let key in person)
+// {
+//     console.log(key);
+//     console.log(person[key]);
+// }
+let student1 = {
+  ID: "A00022",
+  GPA: 3.0,
+  program: "CIT",
+};
+
+let student2 = {
+  ID: "A01000",
+  GPA: 3.1,
+  program: "CST",
+};
+
+let student3 = {
+  ID: "A00114",
+  GPA: 3.2,
+  program: "CIT",
+};
+let students = [student1, student2, student3];
+let counter = 0;
+for (let student of students) {
+  if (student.program === "CIT" && student.GPA > 3) {
+    counter++;
+    console.log(student);
+  }
 }
+console.log(counter);
